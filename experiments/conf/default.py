f29d20e36c1cb4fdd384ee88b1880ecb77bdb1aa
Oswald Berthold <opt@sdfk.de>
2017-04-30 01:30:57 +0200

    cleaning up default conf and comments

--- experiments/conf/default.py
+++ experiments/conf/default.py
@@ -1,4 +1,11 @@
+"""smp_graphs default conf
 
+the config is python, so we
+ - import stuff we need in the config
+ - put the graph config into a dict
+"""
+
+# imports
 from smp_graphs.experiment import make_expr_id
 
 from collections import OrderedDict
@@ -6,12 +13,15 @@ from collections import OrderedDict
 from smp_graphs.block import Block, ConstBlock, UniformRandomBlock
 from smp_graphs.block_plot import TimeseriesPlotBlock
 
-# how can we avoid this?
+# how can we avoid these imports, *?
 from plot import timeseries, histogram
 
+import numpy as np
+
+# reused variables
 numsteps = 100
 
-# graph = [
+# comp. graph
 graph = OrderedDict([
     ("b1", {
         'block': ConstBlock,
@@ -19,7 +29,7 @@ graph = OrderedDict([
             'id': 'b1',
             'idim': None,
             'odim': 3,
-            'const': 1.3
+            'const': np.random.uniform(-1, 1, (3, 1)),
         },
     }),
     ("b2", {
@@ -40,6 +50,7 @@ graph = OrderedDict([
             'blocksize': numsteps,
             'idim': 6,
             'odim': 3,
+            'debug': False,
             'inputs': ['b1', 'b2'],
             'subplots': [
                 [
@@ -50,12 +61,12 @@ graph = OrderedDict([
                     {'inputs': (3, 6), 'plot': timeseries},
                     {'inputs': (3, 6), 'plot': histogram},
                 ],
-#                [(3, 6)]]
             ]
         }
     })
 ])
 
+# top block config, sorry
 conf = {
     'block': Block,
     'params': {
