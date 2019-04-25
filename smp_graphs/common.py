228a06498730e30af51d196661f064bf0142b462
Oswald Berthold <oswald.berthold@gmail.com>
2019-04-25 18:11:35 +0200

    moving stuff from common to smp_base

--- smp_graphs/common.py
+++ smp_graphs/common.py
@@ -1,6 +1,7 @@
 
 import traceback
 import os, sys, pickle, re, ast
+import hashlib
 
 from collections import OrderedDict
 
@@ -10,9 +11,10 @@ from types import FunctionType
 
 import logging
 
-import smp_graphs
+# import smp_graphs
 from smp_base.common import set_attr_from_dict
 from smp_base.common import get_module_logger
+from smp_base.codeops import get_input as get_dict_val
 from smp_graphs.utils import print_dict
 
 logger = get_module_logger(modulename = 'common', loglevel = logging.INFO)
@@ -20,7 +22,6 @@ logger = get_module_logger(modulename = 'common', loglevel = logging.INFO)
 def tuple2inttuple(tpl):
     tpl = tuple((int(_) for _ in tpl))
     return tpl
-
 
 ################################################################################
 # static config templates
@@ -75,31 +76,6 @@ conf = {
 
 # loop_delim = '|'
 loop_delim = '_ll'
-
-################################################################################
-# dynamic code and function mangling
-def code_compile_and_run(code = '', gv = {}, lv = {}, return_keys = []):
-    """Compile and run the code given in the str 'code',
-    optionally including the global and local environments given
-    by 'gv', and 'lv'.
-
-    FIXME: check if 'rkey' is in str code and convert to '%s = %s' % (rkey, code) if it is not
-
-    Returns:
-    - r(dict): lv if |return_keys| = 0, lv[return_keys] otherwise
-    """
-    code_ = compile(code, "<string>", "exec")
-    exec(code, gv, lv)
-    # no keys given, return entire local variables dict
-    if len(return_keys) < 1:
-        return lv
-    # single key given, return just the value of this entry
-    elif len(return_keys) == 1:
-        if return_keys[0] in lv:
-            return lv[return_keys[0]]
-    # several keys given, filter local variables dict by these keys and return
-    else:
-        return dict([(k, lv[k]) for k in return_keys if k in lv])
 
 # AST rewriting
 class RewriteDict(ast.NodeTransformer):
@@ -294,12 +270,10 @@ def get_config_raw_from_string(conf, confvar = 'conf', lconf = None):
 def get_input(inputs, inkey):
     """smp_graphs.common.get_input
 
-    An smp_graphs bus operation: return the 'val' field of the inputs' item at 'inkey'
+    A bus operation, returns the 'val' field of the inputs' item at
+    'inkey' making sure inkey exists in inputs and fail otherwise
     """
-    assert type(inputs) in [dict, OrderedDict]
-    assert inkey is not None
-    assert inkey in inputs
-    return inputs[inkey]['val']
+    return get_dict_val(inputs, inkey)
 
 def compress_loop_id(k):
     if '/' in k: # FIXME: control explicitly with argument
@@ -693,7 +667,6 @@ def md5(obj):
 
     Compute md5 hash for obj using hashlib
     """
-    import hashlib
     # print "self.conf", str(self.conf)
     # if type(obj) is not str:
     #     obj = str(obj)
